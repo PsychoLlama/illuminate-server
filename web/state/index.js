048d9b5eaa4cd8bcbd36f74c26c4f1e3adab27a2
Jesse Gibson <Jesse_Gibson@me.com>
2017-01-15 22:09:25 -0700

    Add redux devtools
    Tryin' out the fancy redux debugging tools. They look all shiny!!!

--- web/state/index.js
+++ web/state/index.js
@@ -7,7 +7,14 @@ export const reducer = combineReducers({
   groups,
 });
 
+const ReduxDevTools = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+let devTools;
+
+if (ReduxDevTools instanceof Function) {
+  devTools = ReduxDevTools();
+}
+
 const initialState = global.__INITIAL_APP_STATE__;
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, devTools);
 
 export default store;
